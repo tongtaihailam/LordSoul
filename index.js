6980ab58bba043fa631e22c75acc972820e20e8e
hoanghailam <enkaiwastaken@gmail.com>
2025-09-02 16:44:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 3000;
-const NEW_URL_PREFIX = process.env.URL_PREFIX || 'https://localhost:3000/';
+const NEW_URL_PREFIX = process.env.URL_PREFIX || 'https://lordsoul.vercel.app/';
 // routers
 app.get('/map_index.json', (req, res) => {
   const filePath = path.join(__dirname, 'files', 'map_index.json');
@@ -15,8 +15,8 @@ app.get('/map_index.json', (req, res) => {
     }
     try {
       let jsonData = JSON.parse(data);
-      if (typeof jsonData.url === 'string' && jsonData.url.startsWith('https://static.resquared.studio/')) {
-        jsonData.url = jsonData.url.replace('https://static.resquared.studio/', NEW_URL_PREFIX);
+      if (typeof jsonData.url === 'string' && jsonData.url.startsWith('https://lordsoul.vercel.app/')) {
+        jsonData.url = jsonData.url.replace('https://lordsoul.vercel.app/', NEW_URL_PREFIX);
       }
       res.json(jsonData);
     } catch (parseErr) {
@@ -27,4 +27,5 @@ app.get('/map_index.json', (req, res) => {
 app.use('/', express.static(path.join(__dirname, 'files')));
 app.use((req, res) => { res.status(404).send('404 Not Found'); });
 // start app
+
 app.listen(PORT);
